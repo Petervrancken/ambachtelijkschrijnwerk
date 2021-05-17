d38661181e2982d177dfac360eb097313056c495
Petervrancken <peter.vrancken88@gmail.com>
2021-05-18 00:13:22 +0200

    Font aangepast, iconen weg, sliderstyle klaar

--- comps/navbar.js
+++ comps/navbar.js
@@ -1,10 +1,5 @@
 // SVG imports!
 import Logo from '../public/logo.svg'
-import Restauratie from '../public/restauratie.svg'
-import Schrijnentimmerwerken from "../public/schrijnentimmerwerken.svg"
-import Treecycleshop from "../public/treecycleshop.svg"
-import Contact from "../public/contact.svg"
-import Login from "../public/login.svg"
 import Winkelkar from "../public/winkelkar.svg"
 import Profiel from "../public/profielicoon.svg"
 import Menu from "../public/menu.svg"
@@ -19,11 +14,11 @@ export default function Navbar() {
     <nav className="navfullscreen">
       <ul className="navbar">
         <li className="marginLogo"><Link href="/"><a title="ga naar home"><Logo className="logo" /></a></Link></li>
-        <li className="linkrestauratie"><Link href="/restauratie"><a title="ga naar restauratie"><Restauratie className="navText"  /></a></Link></li>
-        <li className="linkschrijnentimmerwerken"><Link href="/schrijnentimmerwerken"><a title="ga naar schrijn en timmerwerken"><Schrijnentimmerwerken className="navText" /></a></Link></li>
-        <li className="linktreecycle"><Link href="/treecycle"><a title="ga naar treecyleshop"><Treecycleshop className="navText"  /></a></Link></li>
-        <li className="linkcontact"><Link href="/contact"><a title="ga naar contact"><Contact className="navText" /></a></Link></li>
-        <li className="linklogin"><Link href="/login"><a title="ga naar login"><Login className="navText" /></a></Link></li>
+        <li className="linkrestauratie"><Link href="/restauratie"><a title="ga naar restauratie"><p>restauratie</p></a></Link></li>
+        <li className="linkschrijnentimmerwerken"><Link href="/schrijnentimmerwerken"><a title="ga naar schrijn en timmerwerken"><p>schrijn en timmerwerken</p></a></Link></li>
+        <li className="linktreecycle"><Link href="/treecycle"><a title="ga naar treecyleshop"><p>treecycleshop</p></a></Link></li>
+        <li className="linkcontact"><Link href="/contact"><a title="ga naar contact"><p>contact</p></a></Link></li>
+        <li className="linklogin"><Link href="/login"><a title="ga naar login"><p>login</p></a></Link></li>
         <li className="linkwinkelkar"><Link href="/winkelkar"><a title="ga naar winkelkar"><Winkelkar className="icoon" /></a></Link></li>
         <li className="linkprofiel"><Link href="/profiel"><a title="ga naar profiel"><Profiel className="icoon"  /></a></Link></li>
       </ul>
@@ -43,11 +38,11 @@ export default function Navbar() {
         <div className="dropdown">
           <div className="linkmenu"><Menu className="menuicoon" height={25} /></div>
           <div className="dropdown-content">
-            <Link href="/restauratie"><a title="ga naar restauratie"><Restauratie className="navText"  /></a></Link>
-            <Link href="/schrijnentimmerwerken"><a title="ga naar schrijn en timmerwerken"><Schrijnentimmerwerken className="navText" /></a></Link>
-            <Link href="/treecycle"><a title="ga naar treecyleshop"><Treecycleshop className="navText"  /></a></Link>
-            <Link href="/contact"><a title="ga naar contact"><Contact className="navText" /></a></Link>
-            <Link href="/login"><a title="ga naar login"><Login className="navText" /></a></Link>
+            <Link href="/restauratie"><a title="ga naar restauratie"><p className="textdropdown">restauratie</p></a></Link>
+            <Link href="/schrijnentimmerwerken"><a title="ga naar schrijn en timmerwerken"><p className="textdropdown">schrijn en timmerwerken</p></a></Link>
+            <Link href="/treecycle"><a title="ga naar treecyleshop"><p className="textdropdown">treecycleshop</p></a></Link>
+            <Link href="/contact"><a title="ga naar contact"><p className="textdropdown">contact</p></a></Link>
+            <Link href="/login"><a title="ga naar login"><p className="textdropdown">login</p></a></Link>
           </div>
         </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,39 +1,31 @@
 
-import Wijmaken from "../public/wijmaken.svg"
-import Geschiedenis from "../public/geschiedenis.svg"
-import Vanonze  from "../public/vanonze.svg"
-import Toekomst from "../public/toekomst.svg"
 import Profiel from "../public/profielicoon.svg"
 
-
+// import Swiper bundle
 import Swiper from 'swiper/bundle';
 import "swiper/swiper-bundle.css"
 
 
 export default function Home() {
 
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
+  const swiper = new Swiper('.swiper-container', {
+    
     spaceBetween: 30,
-    slidesPerGroup: 1,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+      el: '.swiper-scrollbar',
     },
     breakpoints: {
       1600: {
         slidesPerView: 4,
       },
-      900: {
+      1200: {
         slidesPerView: 3,
       },
-      600: {
+      790: {
         slidesPerView: 2,
       },
       10: {
@@ -47,122 +39,67 @@ export default function Home() {
   return (
     <div className="backgroundTable">
       <div className="slogan">
-        <Wijmaken className="sloganText"/>
-        <Geschiedenis className="sloganText"/>
-        <Vanonze className="sloganTextVanOnze"/>
-        <Toekomst className="sloganTextToekomst"/>
+        <p className="sloganText">wij maken geschiedenis van onze toekomst!</p>
       </div>
-      <div class="swiper-container mySwiper">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide">
-          <div className="swiper-icoon">
-            <div className="swiper-icoon-grades">
-              <p>27/08/2021</p>
-              <h3 className="swiper-grades">10/10</h3>
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div className="swiper-slide">
+            <div className="swiper-icoon">
+              <div className="swiper-icoon-grades">
+                <p>27/08/2021</p>
+                <h3 className="swiper-grades">10/10</h3>
+              </div>
+              <div className="swiper-icoon-grades">
+                <Profiel className="icoon"  />
+                <p>Peter Vrancken</p>
+              </div>
+              <p className="swiper-comment">Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
             </div>
-            <div className="swiper-icoon-grades">
-              <Profiel className="icoon"  />
-              <p>Peter Vrancken</p>
+          </div>
+          <div className="swiper-slide">
+            <div className="swiper-icoon">
+              <div className="swiper-icoon-grades">
+                <p>27/08/2021</p>
+                <h3 className="swiper-grades">10/10</h3>
+              </div>
+              <div className="swiper-icoon-grades">
+                <Profiel className="icoon"  />
+                <p>Peter Vrancken</p>
+              </div>
+              <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
             </div>
-            <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
+          </div>
+          <div className="swiper-slide">
+            <div className="swiper-icoon">
+              <div className="swiper-icoon-grades">
+                <p>27/08/2021</p>
+                <h3 className="swiper-grades">10/10</h3>
+              </div>
+              <div className="swiper-icoon-grades">
+                <Profiel className="icoon"  />
+                <p>Peter Vrancken</p>
+              </div>
+              <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
+            </div>
+          </div>
+          <div className="swiper-slide">
+            <div className="swiper-icoon">
+              <div className="swiper-icoon-grades">
+                <p>27/08/2021</p>
+                <h3 className="swiper-grades">10/10</h3>
+              </div>
+              <div className="swiper-icoon-grades">
+                <Profiel className="icoon"  />
+                <p>Peter Vrancken</p>
+              </div>
+              <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
+            </div>
           </div>
         </div>
-        <div class="swiper-slide">
-          <div className="swiper-icoon">
-            <div className="swiper-icoon-grades">
-              <p>27/08/2021</p>
-              <h3 className="swiper-grades">10/10</h3>
-            </div>
-            <div className="swiper-icoon-grades">
-              <Profiel className="icoon"  />
-              <p>Marianna Adamou</p>
-            </div>
-            <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div className="swiper-icoon">
-            <div className="swiper-icoon-grades">
-              <p>27/08/2021</p>
-              <h3 className="swiper-grades">10/10</h3>
-            </div>
-            <div className="swiper-icoon-grades">
-              <Profiel className="icoon"  />
-              <p>Michiel Vandenbroecke</p>
-            </div>
-            <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div className="swiper-icoon">
-            <div className="swiper-icoon-grades">
-              <p>27/08/2021</p>
-              <h3 className="swiper-grades">10/10</h3>
-            </div>
-            <div className="swiper-icoon-grades">
-              <Profiel className="icoon"  />
-              <p>Tim Van Mierlo</p>
-            </div>
-            <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div className="swiper-icoon">
-            <div className="swiper-icoon-grades">
-              <p>27/08/2021</p>
-              <h3 className="swiper-grades">10/10</h3>
-            </div>
-            <div className="swiper-icoon-grades">
-              <Profiel className="icoon"  />
-              <p>Charel Jos</p>
-            </div>
-            <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div className="swiper-icoon">
-            <div className="swiper-icoon-grades">
-              <p>27/08/2021</p>
-              <h3 className="swiper-grades">10/10</h3>
-            </div>
-            <div className="swiper-icoon-grades">
-              <Profiel className="icoon"  />
-              <p>Iemand</p>
-            </div>
-            <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div className="swiper-icoon">
-            <div className="swiper-icoon-grades">
-              <p>27/08/2021</p>
-              <h3 className="swiper-grades">10/10</h3>
-            </div>
-            <div className="swiper-icoon-grades">
-              <Profiel className="icoon"  />
-              <p>De bomma</p>
-            </div>
-            <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
-          </div>
-        </div>
-        <div class="swiper-slide">
-          <div className="swiper-icoon">
-            <div className="swiper-icoon-grades">
-              <p>27/08/2021</p>
-              <h3 className="swiper-grades">10/10</h3>
-            </div>
-            <div className="swiper-icoon-grades">
-              <Profiel className="icoon"  />
-              <p>Sonic</p>
-            </div>
-            <p>Anton en Kristof hebben zeer goed werk afgeleverd! Ik kan vol lof genieten van mijn op maat gemaakte trap!</p>
-          </div>
-        </div>
+        <div class="swiper-pagination"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
       </div>
-      <div class="swiper-button-next"></div>
-      <div class="swiper-button-prev"></div>
-      <div class="swiper-pagination"></div>
-    </div>
     </div>
   )
 }

6b2d3a8e62d37d9919453741c35674f3657d291b
Petervrancken <peter.vrancken88@gmail.com>
2021-08-30 22:56:40 +0200

    frontpage downgrade rdy

--- comps/footer.js
+++ comps/footer.js
@@ -1,13 +1,23 @@
 // SVG imports!
-import Logo from "../public/logo.svg";
-import Menu from "../public/menu.svg";
+import Instagram from "../public/instagram.svg";
+import Facebook from "../public/facebook2.svg";
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <>
       <div className="footer">
-        <p>Footer</p>
+        <Link href="https://www.instagram.com/destrooperwoodworks/">
+          <a title="onze instagram">
+            <Instagram className="instagram-logo" />
+          </a>
+        </Link>
+        <p className="footer-text">Volg ons</p>
+        <Link href="https://m.facebook.com/destrooperwoodworks/">
+          <a title="onze facebook">
+            <Facebook className="instagram-logo" />
+          </a>
+        </Link>
       </div>
     </>
   );

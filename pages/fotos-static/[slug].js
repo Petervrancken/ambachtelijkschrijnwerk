06a54837f14f175f78dc78aab4f855c99d8cec19
Petervrancken <peter.vrancken88@gmail.com>
2021-09-10 16:01:39 +0200

    Fixed photo's without database

---
+++ pages/fotos-static/[slug].js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import router, { useRouter } from "next/router";
+import Image from "next/image";
+
+//Verander hier je URL vergeet niet op deze pagina ook je foto url aan te passen
+const URL = "https://wdev2.be/peter21/eindwerk"; // wdev url
+//const URL = "https://127.0.0.1:8000"; // local url
+
+// De foto met de gevraagde id komt hier binnen.
+export default function fotoDetail() {
+  const router = useRouter();
+  const { slug } = router.query;
+  return (
+    <>
+      <div className="backgroundTableTheme">
+        <p onClick={() => router.back()} className="previous">
+          Terug
+        </p>
+        <div className="sloganTheme">
+          <div className="foto-size">
+            <Image
+              className="foto-vergroot"
+              src={`/Bigphoto/${slug}`}
+              width={1000}
+              height={1000}
+              alt="fotovergroting"
+            ></Image>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- pages/restauratie.js
+++ pages/restauratie.js
@@ -2,6 +2,7 @@
 import Timmerwerk from "../public/timmer.svg";
 import Zaag from "../public/zaag.svg";
 import Veiligheid from "../public/veiligheid.svg";
+import Image from "next/image";
 
 import axios from "axios";
 
@@ -18,26 +19,21 @@ import "swiper/swiper-bundle.min.css";
 const URL = "https://wdev2.be/peter21/eindwerk"; // wdev url
 //const URL = "https://127.0.0.1:8000"; // local url
 
-export default function Restauratie(restoProps) {
-  //Put all foto's in an array.
-  const fotos = restoProps.restoProps.fotos;
-  //sorteerd op datum/id
-  fotos.sort((a, b) => b.id - a.id);
-
-  //Put all topics in an array.
-  const onderwerpen = restoProps.restoProps.onderwerp;
-
+export default function Restauratie() {
   return (
     <>
       <div className="backgroundTableTheme">
         <div className="sloganTheme">
-          <p className="sloganText">{restoProps.restoProps.titel}</p>
-          <p
-            dangerouslySetInnerHTML={{
-              __html: restoProps.restoProps.beschrijving,
-            }}
-            className="infoRestauratie"
-          ></p>
+          <p className="sloganText">restauratie</p>
+          <p className="infoRestauratie">
+            Aangezien we een grote liefde hebben voor het vakmanschap van
+            weleer, hebben we ons gespecialiseerd in het restaureren van antiek
+            houtwerk. Zoals bij alle restauratiewerken trachten we zo veel
+            mogelijk van de originele timmerwerken te behouden. We voeren de
+            herstellingen uit met de aloude technieken zoals liplassen of
+            zwaluwstaarten. Ook doen we herstellingen met moderne technieken
+            zoals polymeerchemisch en glasvezel.
+          </p>
         </div>
 
         <Swiper
@@ -69,97 +65,187 @@ export default function Restauratie(restoProps) {
           // elke foto uitprinten met juiste fotonaam in zijn path
           className="mySwiper"
         >
-          {fotos.length > 0 &&
-            fotos.map((oneFoto) => {
-              const fotoPath =
-                "https://wdev2.be/peter21/eindwerk/system/image.php/" +
-                oneFoto.fotonaam +
-                "?image=/peter21/eindwerk/images/afbeeldingen/" +
-                oneFoto.fotonaam;
-
-              return (
-                <SwiperSlide key={oneFoto.id} className="slide-afbeelding">
-                  <div className="swiper-afbeelding">
-                    <Link href={"/fotos-static/" + String(oneFoto.id)}>
-                      <a title="klik en vergroot!">
-                        <img
-                          height={250}
-                          width={250}
-                          src={fotoPath}
-                          alt="Don't forget your alt text"
-                        />
-                      </a>
-                    </Link>
-                  </div>
-                </SwiperSlide>
-              );
-            })}
+          <SwiperSlide key="afbeelding1" className="slide-afbeelding">
+            <div className="swiper-afbeelding">
+              <Link href="/fotos-static/restauratie-afbeelding1.jpg">
+                <a title="klik en vergroot!">
+                  <Image
+                    height={250}
+                    width={250}
+                    src="/SmallPhoto/restauratie-afbeelding1.jpg"
+                    alt="Don't forget your alt text"
+                  />
+                </a>
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide key="afbeelding2" className="slide-afbeelding">
+            <div className="swiper-afbeelding">
+              <Link href="/fotos-static/restauratie-afbeelding2.jpg">
+                <a title="klik en vergroot!">
+                  <Image
+                    height={250}
+                    width={250}
+                    src="/SmallPhoto/restauratie-afbeelding2.jpg"
+                    alt="Don't forget your alt text"
+                  />
+                </a>
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide key="afbeelding3" className="slide-afbeelding">
+            <div className="swiper-afbeelding">
+              <Link href="/fotos-static/restauratie-afbeelding3.jpg">
+                <a title="klik en vergroot!">
+                  <Image
+                    height={250}
+                    width={250}
+                    src="/SmallPhoto/restauratie-afbeelding3.jpg"
+                    alt="Don't forget your alt text"
+                  />
+                </a>
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide key="afbeelding4" className="slide-afbeelding">
+            <div className="swiper-afbeelding">
+              <Link href="/fotos-static/restauratie-afbeelding4.jpg">
+                <a title="klik en vergroot!">
+                  <Image
+                    height={250}
+                    width={250}
+                    src="/SmallPhoto/restauratie-afbeelding4.jpg"
+                    alt="Don't forget your alt text"
+                  />
+                </a>
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide key="afbeelding5" className="slide-afbeelding">
+            <div className="swiper-afbeelding">
+              <Link href="/fotos-static/restauratie-afbeelding5.jpg">
+                <a title="klik en vergroot!">
+                  <Image
+                    height={250}
+                    width={250}
+                    src="/SmallPhoto/restauratie-afbeelding5.jpg"
+                    alt="Don't forget your alt text"
+                  />
+                </a>
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide key="afbeelding6" className="slide-afbeelding">
+            <div className="swiper-afbeelding">
+              <Link href="/fotos-static/restauratie-afbeelding6.jpg">
+                <a title="klik en vergroot!">
+                  <Image
+                    height={250}
+                    width={250}
+                    src="/SmallPhoto/restauratie-afbeelding6.jpg"
+                    alt="Don't forget your alt text"
+                  />
+                </a>
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide key="afbeelding7" className="slide-afbeelding">
+            <div className="swiper-afbeelding">
+              <Link href="/fotos-static/restauratie-afbeelding7.jpg">
+                <a title="klik en vergroot!">
+                  <Image
+                    height={250}
+                    width={250}
+                    src="/SmallPhoto/restauratie-afbeelding7.jpg"
+                    alt="Don't forget your alt text"
+                  />
+                </a>
+              </Link>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide key="afbeelding8" className="slide-afbeelding">
+            <div className="swiper-afbeelding">
+              <Link href="/fotos-static/restauratie-afbeelding8.jpg">
+                <a title="klik en vergroot!">
+                  <Image
+                    height={250}
+                    width={250}
+                    src="/SmallPhoto/restauratie-afbeelding8.jpg"
+                    alt="Don't forget your alt text"
+                  />
+                </a>
+              </Link>
+            </div>
+          </SwiperSlide>
+          );
         </Swiper>
 
         <div className="onderwerp">
           <div className="box">
-            <p className="onderwerp-titel">{onderwerpen[0].titel}</p>
+            <p className="onderwerp-titel">Restauratietimmerwerk</p>
             <Timmerwerk className="onderwerp-icoon" />
-            <div
-              className="swiper-comment"
-              dangerouslySetInnerHTML={{ __html: onderwerpen[0].beschrijving }}
-            ></div>
+            <p className="swiper-comment">
+              Dakstrukturen en andere structurele elementen (bv. moerbalken) in
+              monumenten of andere oude gebouwen zijn meestal gemaakt in hout.
+              Hoewel hout een zeer duurzaam bouwmateriaal is, kan na jarenlange
+              blootstelling aan de elementen of door aantasting van beestjes,
+              restauratie van doen zijn.{" "}
+            </p>
           </div>
           <div className="box">
-            <p className="onderwerp-titel">{onderwerpen[1].titel}</p>
+            <p className="onderwerp-titel">Restauratieschrijnwerk</p>
             <Zaag className="onderwerp-icoon" />
-            <div
-              className="swiper-comment"
-              dangerouslySetInnerHTML={{ __html: onderwerpen[1].beschrijving }}
-            ></div>
+            <p className="swiper-comment">
+              Dit gaat van ramen en deuren tot trappen en ander interieur
+              schrijnwerk. We streven steeds na zo veel mogelijk van het
+              originele schrijnwerk te behouden, enkel delen die niet meer gered
+              kunnen worden volgens de regels van de kunst geheel of
+              gedeeltelijk vervangen.
+            </p>
           </div>
           <div className="box">
-            <p className="onderwerp-titel">{onderwerpen[2].titel}</p>
+            <p className="onderwerp-titel">Bereikbaarheid en veiligheid</p>
             <Veiligheid className="onderwerp-icoon" />
-            <div
-              className="swiper-comment"
-              dangerouslySetInnerHTML={{ __html: onderwerpen[2].beschrijving }}
-            ></div>
+            <p className="swiper-comment">
+              Platformen en loopbruggen om moeilijk bereikbare plaatsen toch
+              gemakkelijk en veilig te kunnen bereiken kunnen we ook
+              vervaardigen.
+            </p>
           </div>
         </div>
       </div>
       <div className="onderwerp-bottom">
         <div className="box">
-          <p className="onderwerp-titel">{onderwerpen[0].titel}</p>
+          <p className="onderwerp-titel">Restauratietimmerwerk</p>
           <Timmerwerk className="onderwerp-icoon" />
-          <div
-            className="swiper-comment"
-            dangerouslySetInnerHTML={{ __html: onderwerpen[0].beschrijving }}
-          ></div>
+          <p className="swiper-comment">
+            Dakstrukturen en andere structurele elementen (bv. moerbalken) in
+            monumenten of andere oude gebouwen zijn meestal gemaakt in hout.
+            Hoewel hout een zeer duurzaam bouwmateriaal is, kan na jarenlange
+            blootstelling aan de elementen of door aantasting van beestjes,
+            restauratie van doen zijn.{" "}
+          </p>
         </div>
         <div className="box">
-          <p className="onderwerp-titel">{onderwerpen[1].titel}</p>
+          <p className="onderwerp-titel">Restauratieschrijnwerk</p>
           <Zaag className="onderwerp-icoon" />
-          <div
-            className="swiper-comment"
-            dangerouslySetInnerHTML={{ __html: onderwerpen[1].beschrijving }}
-          ></div>
+          <p className="swiper-comment">
+            Dit gaat van ramen en deuren tot trappen en ander interieur
+            schrijnwerk. We streven steeds na zo veel mogelijk van het originele
+            schrijnwerk te behouden, enkel delen die niet meer gered kunnen
+            worden volgens de regels van de kunst geheel of gedeeltelijk
+            vervangen.
+          </p>
         </div>
         <div className="box">
-          <p className="onderwerp-titel">{onderwerpen[2].titel}</p>
+          <p className="onderwerp-titel">Bereikbaarheid en veiligheid</p>
           <Veiligheid className="onderwerp-icoon" />
-          <div
-            className="swiper-comment"
-            dangerouslySetInnerHTML={{ __html: onderwerpen[2].beschrijving }}
-          ></div>
+          <p className="swiper-comment">
+            Platformen en loopbruggen om moeilijk bereikbare plaatsen toch
+            gemakkelijk en veilig te kunnen bereiken kunnen we ook vervaardigen.
+          </p>
         </div>
       </div>
     </>
   );
-}
-
-export async function getStaticProps() {
-  const resp = await axios(URL + "/api/themas/1.json");
-  const restoProps = resp.data;
-  return {
-    props: {
-      restoProps,
-    },
-    revalidate: 3600,
-  };
 }
